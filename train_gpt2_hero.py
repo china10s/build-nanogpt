40f8da50cda1fe3022fe4c2a1881f03879d60862
techzhu <techzhu@tencent.com>
2024-06-26 09:23:20 +0800

    learn to load weight from pretrain model

--- train_gpt2_hero.py
+++ train_gpt2_hero.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import math
 
 # ---------------------------------------------------------------
 
@@ -32,7 +33,7 @@ class CausalSelfAttention(nn.Module):
         att = att.masked_fill(self.bias[:,:,:T,:T] == 0,float('-inf')) # 将下三角区域mask起来，赋值为无穷小
         att = F.softmax(att, dim=-1)
         y = att @ v # [B, nh, T, T]x[B, nh, T, hs] -> [B, nh, T, hs] softmax结果与V矩阵做内积
-        y = y.transopse(1,2).contiguous().view(B, T, C) #将全部的head一个个concat起来
+        y = y.transpose(1,2).contiguous().view(B, T, C) #将全部的head一个个concat起来
         y = self.c_proj(y)
         return y
 
@@ -102,7 +103,7 @@ class GPT(nn.Module):
         logits = self.lm_head(x)
         loss = None
         if targets is not None:
-            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1)) # 计算交叉熵
         return logits, loss
 
 
@@ -154,6 +155,48 @@ class GPT(nn.Module):
 
         return model
 
+# ------------------------------------------------------------------------
+device= "cpu"
+# if torch.cuda.is_available():
+#     device = "cuda"
+# elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+#     device = "mps"
+print(f"using device: {device}")
+
+num_return_sequences = 5
+max_length = 30
 
 model = GPT.from_pretrained('gpt2')
+# model = GPT(GPTConfig()) # 如果是随机初始化的参数，则输出结果也会是无意义的
+model.eval()
+model.to(device)
 print("didn't crash yey")
+
+
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello, I'm a language model")
+# tokens = enc.encode("你好，我是一个语言模型")
+tokens = torch.tensor(tokens, dtype=torch.long )
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # 将1D的tensor扩展到2D，为了增加batchsize维度，虽然这里batchsize=1；同时，也复制‘num_return_sequences’份，以产生多个返回结果
+
+x = tokens.to(device)
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1) < max_length: # 未达到最大长度之前，一直生成next token
+    # model 推理，计算出logit
+    with torch.no_grad(): # 确保每次推理，都清理掉上次推理残留的梯度
+        logits = model(x)
+        # print(logits)
+        logits = logits[0][:, -1, :] # 取出返回结果中的【最后一个token】（也就是即将生成的token）的logits
+        probs = F.softmax(logits, dim = -1) # 计算所有token emb库中，softmax后的全部结果
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1) # huggingface 默认topK=50
+        ix = torch.multinomial(topk_probs, 1) # 从多项式分布中，提取1个样本
+        xcol = torch.gather(topk_indices, -1, ix) # 获取胜选的token
+        x = torch.cat((x, xcol), dim=-1) #将生成的新的token加入x中
+
+for i in range(num_return_sequences): # 获取每一个返回的句子里全部的token，并解码成字符串
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
